54fef67bd42fac9949c6e31834ebc5e68d053cfe
Dan Kagan <dankagan7@gmail.com>
2019-04-22 02:25:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -22,7 +22,7 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
         if flavor == "strawberry"
-          valeu.delete("strawberry")
+          value.delete("strawberry")
         end
       end
       contacts["Jon Snow"][:favorite_icecream_flavors]
